cab49280b18c727b86a4bd926919d6bf54869da2
Tom M. Ragonneau <tom.ragonneau@connect.polyu.hk>
2021-08-24 11:14:02 +0800

    Add download counts retrived date

--- static/scripts/stats/counts.py
+++ static/scripts/stats/counts.py
@@ -1,5 +1,6 @@
 import json
 import sys
+from datetime import date
 
 import dask.dataframe as dd
 import pypistats
@@ -33,9 +34,11 @@ def github_count(user, package):
 
 
 if __name__ == '__main__':
+    today = date.today()
     with open(sys.argv[1], 'w') as fd:
         json.dump({
             'conda': conda_count('pdfo'),
             'pypi': pypi_count('pdfo'),
             'github': github_count('pdfo', 'pdfo'),
+            'update': today.strftime('%B %d, %Y'),
         }, fd)
