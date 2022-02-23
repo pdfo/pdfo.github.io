56ce01d3fcbf3b5e2ce797d3a15dc68d883bd22e
Tom M. Ragonneau <t.ragonneau@gmail.com>
2022-02-23 13:26:49 +0800

    Correct counting script

--- static/scripts/stats/counts.py
+++ static/scripts/stats/counts.py
@@ -2,29 +2,21 @@ import json
 import sys
 from datetime import date
 
-import dask.dataframe as dd
+import condastats.cli
 import pypistats
 import requests
 
 
 def conda_count(package):
     """Download count through the Anaconda distribution."""
-    df = dd.read_parquet(
-        's3://anaconda-package-data/conda/monthly/*/*.parquet',
-        storage_options={'anon': True},
-    )
-    df = df.query(f'pkg_name == "{package}"')
-    df = df.compute()
-    df['pkg_name'] = df['pkg_name'].cat.remove_unused_categories()
-    return int(df.counts.sum())
+    s = condastats.cli.overall(package)
+    return int(s[package])
 
 
 def pypi_count(package):
     """Download count through the PyPI distribution."""
-    df = pypistats.overall(package, format='pandas')
-    df = df.loc[df['category'] == 'without_mirrors']
-    df.reset_index(drop=True, inplace=True)
-    return int(df.at[0, 'downloads'])
+    df = pypistats.overall(package, mirrors=False, format='pandas')
+    return int(df['downloads'].iloc[0])
 
 
 def github_count(user, package):
